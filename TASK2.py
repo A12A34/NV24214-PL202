7fe5de1469635adb8ecd8e38fae03ae9a537fe98
Ali Alhasan <arady1717@gmail.com>
2026-02-11 11:51:20 +0300

    Remove display.py and add TASK1.py, TASK2.py, TASK3.py with example outputs; update settings.json for strict type checking

---
+++ TASK2.py
@@ -0,0 +1,11 @@
+a = [1,2,2,3,4,4,5]
+a.append(6)
+print(a)
+del a[0]
+print(a)
+a.remove(2)
+print(a)
+a.insert(0,1)
+print(a)
+a.sort()
+print(a)
---
+++ TASK3.py
@@ -0,0 +1,8 @@
+a = {1: "one", 2: "two", 3: "three"}
+a[4] = "four"
+print(a)
+del a[2]
+print(a)
+print(a.values())
+print(a.keys())
+print(a.items())
